d57ba26a587a0e54ce127781f239a1f989fa9638
Christine Nathaniel <cnathaniel@paycor.com>
2022-08-19 10:08:36 -0700

    heading, logo and submit form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import groceryimage from './Groceries.jpeg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [firstName, setFirstName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('You have submitted');
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Stine's Grocery List 
+        </h1>
+        <img src={groceryimage} className="App-logo" alt="logo" />
       </header>
+      <main>
+        <form className='form'>
+          <div className='form-control'>
+              <label htmlFor='firstName'>Name: </label>
+              <input
+                  type='text'
+                  id='firstName'
+                  name='firstName'
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+              />
+          </div>
+          <button type='submit' onClick={handleSubmit}>Add item</button>
+        </form>
+      </main>
     </div>
   );
 }
