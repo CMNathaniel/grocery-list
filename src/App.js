b0678de26e4af3c18e1b16025a2557684c8c2757
Christine Nathaniel <cnathaniel@paycor.com>
2022-08-22 14:29:44 -0700

    in the weeds with state to manage list of items

--- src/App.js
+++ src/App.js
@@ -6,12 +6,35 @@ const App = () => {
 
   const [listItem, setListItem] = useState('');
   const [submittedItem, setSubmittedItem] = useState(null);
+  const [allSubmittedItems, setAllSubmittedItems] = useState([]);
+
+  // const listOfSubmittedItems = [
+  //   // "carrots",
+  //   // "peas",
+  //   // "onions",
+  //   // "bread"
+  // ];
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setSubmittedItem(listItem);
-    setListItem("");
+    console.log(allSubmittedItems);
+    allSubmittedItems.push(submittedItem);
+    // so right now the array only gets the new item added after 2 user clicks. But even then, the new array is not getting pushed into the DOM
+    // after each 
+    console.log(`listof items after .push() ${allSubmittedItems}`);
+    console.log(`length of listOfSubmittedItems after .push ${allSubmittedItems.length}`)
+    // setListItem("");
   }
+
+  
+
+  // is my key as the index causing any problems? Should I change that to something else?
+  const renderListOfSubmittedItems = allSubmittedItems.map((item, index) =>
+    <ul>
+      <li key={index}>{item}</li>
+    </ul>
+  );
 
   return (
     <div className="App">
@@ -38,8 +61,13 @@ const App = () => {
         </form>
         <div class="grocery-list">
           <h3>My Grocery List</h3>
+          <p>Total Grocery List Items: {allSubmittedItems.length}</p>
           <ul>
+        
+            <li>Below is the user inputted item</li>
             <li>{submittedItem}</li>
+            <li>Below is the currently hardcoded array of items</li>
+            <li>{renderListOfSubmittedItems}</li>
           </ul>
         </div>
       </main>
