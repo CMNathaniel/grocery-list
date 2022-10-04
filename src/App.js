ec48d5a60713e36ff7d3d1fa4642843b98a76144
Christine Nathaniel <cnathaniel@paycor.com>
2022-10-04 09:06:24 -0700

    cleaning up CSS a little, adding commentary to main functionality

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import groceryimage from './Groceries.jpeg';
 
 
 export default function App() {
+  // hook with initial empty array groceryItems.name we'll use to populate the list of grocery items
   const [groceryItems, setGroceryItems] = useState({
     name: []
   });
 
+  // hook for adding user inputted data from form into the groceryItems.name array. 
+  // form field starts with empty string until user types something 
   const [newGroceryItem, setNewGroceryItem] = useState("");
 
+  // callback function to pass user typed data "onChange" from form into newGroceryItem
   const updateNewGroceryItem = (event) => {
     setNewGroceryItem(event.target.value);
   };
 
+  // callback function to user submitted data "onSubmit" from form into groceryItems.name array
+  // prevents page refresh, then spreads any existing items in our array, plus adds the newGroceryItem
   const addGroceryItem = (event) => {
     event.preventDefault();
     setGroceryItems(groceryItem => (
@@ -22,16 +28,17 @@ export default function App() {
       name: [...groceryItem.name, newGroceryItem]
       }
     ));
+    // we return the form to an empty state afterwards 
     setNewGroceryItem("");
   };
   
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      <header className="app-header">
         <h1>
           Stine's Grocery List ☑ 
         </h1>
-        <img src={groceryimage} className="App-logo" alt="logo" />
+        <img src={groceryimage} className="app-logo" alt="logo" />
       </header>
       <main>
         <form className="form" onSubmit={addGroceryItem} >
@@ -45,10 +52,12 @@ export default function App() {
                   value={newGroceryItem}
                   onChange={updateNewGroceryItem}
               />
+              <button type="submit">Add</button>
           </div>
-          <button type="submit">Add to List</button>
         </form>
         <div className="grocery-list">
+          {/* map method iterates through the array groceryItems.name and creates each as a "ul" element 
+          unique key required for each child so we are using "data" */}
           <div>
             {groceryItems.name.map((data) => (
               <ul key={data}>{data}</ul>
