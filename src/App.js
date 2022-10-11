54b69af80370271b0e0a86c1653b712071e7d8a1
Christine Nathaniel <cnathaniel@paycor.com>
2022-10-11 08:17:09 -0700

    making the key value in .map() method unique, includes index now

--- src/App.js
+++ src/App.js
@@ -57,10 +57,10 @@ export default function App() {
         </form>
         <div className="grocery-list">
           {/* map method iterates through the array groceryItems.name and creates each as a "ul" element 
-          unique key required for each child so we are using "data" */}
+          unique key required for each child so we are using "data" + index of the item */}
           <div>
-            {groceryItems.name.map((data) => (
-              <ul key={data}>{data}</ul>
+            {groceryItems.name.map((data, i) => (
+              <ul key={'data_' + i}>{data}</ul>
             ))}
           </div>
         </div>
